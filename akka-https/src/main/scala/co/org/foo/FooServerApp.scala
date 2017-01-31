69718fb5905b99bac078b59da048da174b690e1c
jsrestrepom <santyrestrepom@gmail.com>
2017-01-30 22:24:02 -0500

    akka-http autocontent app writing in scala with json support for translate model

--- akka-https/src/main/scala/co/org/foo/FooServerApp.scala
+++ akka-https/src/main/scala/co/org/foo/FooServerApp.scala
@@ -1,16 +1,16 @@
 package co.org.foo
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives.{path, get, complete}
-import akka.stream.ActorMaterializer
 
-import scala.io.StdIn
+object WebServer {
+  import akka.actor.ActorSystem
+  import akka.http.scaladsl.Http
+  import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+  import akka.http.scaladsl.server.Directives.{path, get, complete}
+  import akka.stream.ActorMaterializer
 
-object FooServerApp {
-
+  import scala.io.StdIn
   def main(args: Array[String]): Unit = {
+
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
@@ -21,7 +21,68 @@ object FooServerApp {
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-httpj</h1>"))
         }
       }
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine()
+    bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
+  }
+
+}
+
+
+object WebServerWithModel {
+  import akka.actor.ActorSystem
+  import akka.http.scaladsl.Http
+  import akka.stream.ActorMaterializer
+  import akka.Done
+  import akka.http.scaladsl.model.StatusCodes
+  import akka.http.scaladsl.server.Directives._
+  import akka.http.scaladsl.server.PathMatchers.LongNumber
+  import akka.http.scaladsl.server.Route
+  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+  import spray.json.DefaultJsonProtocol._
+
+  import scala.io.StdIn
+
+  import scala.concurrent.Future
+
+  final case class Item(name: String, id: Long)
+  final case class Order(items: List[Item])
+
+  implicit val itemFormat = jsonFormat2(Item)
+  implicit val orderFormat = jsonFormat1(Order)
+
+  def fetchItem(itemId: Long): Future[Option[Item]] = ???
+  def saveOrder(order: Order): Future[Done] = ???
+
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
+
+    val route: Route =
+      get {
+        pathPrefix("item" / LongNumber) { id =>
+          val maybeItem: Future[Option[Item]] = fetchItem(id)
+
+          onSuccess(maybeItem) {
+            case Some(item) => complete(item)
+            case None => complete(StatusCodes.NotFound)
+          }
+        }
+      } ~
+      post {
+        path("create-order") {
+          entity(as[Order]) { order =>
+            val saved: Future[Done] = saveOrder(order)
+
+            onComplete(saved) { done =>
+              complete("order created")
+            }
+          }
+      }
+    }
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
